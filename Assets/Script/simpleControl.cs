3e60dc52e67d8e47d2fbbf852cc5b5f86b6ec39f
swaraadam <bimaswaraadam@gmail.com>
2018-06-02 12:58:20 +0700

    Add Physic and Manual Control
    -Physic Car
    - Car Controlling

---
+++ Assets/Script/simpleControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleControl : MonoBehaviour {
+
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the otor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach(AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+	}
+
+    [System.Serializable]
+    public class AxleInfo
+    {
+        public WheelCollider leftWheel;
+        public WheelCollider rightWheel;
+        public bool motor; //cek, nyambung ama motor nggak?
+        public bool steering; //cek, bisa disetir nggak?
+    }
+}
