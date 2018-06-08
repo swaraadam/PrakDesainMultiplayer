c2b2886b96e0d5f59262da77d81732e8ea2fc478
swaraadam <bimaswaraadam@gmail.com>
2018-06-08 21:14:49 +0700

    Add Client Server System
    Next : Sync With Gameplay

---
+++ Assets/Script/Network/ClientUDP.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Threading;
+using System.Net;
+using UnityEngine;
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+public class ClientUDP : MonoBehaviour {
+
+    PlayerStat player1, player2, tempPlayer, Me;
+    UdpClient udpClient;
+    IPEndPoint iPEndPoint;
+
+
+	// Use this for initialization
+	void Start () {
+        player1 = new PlayerStat(0, 0, false, false);
+        player2 = new PlayerStat(0, 0, false, false);
+        tempPlayer = new PlayerStat(0, 0, false, false);
+        Me = new PlayerStat(0, 0, false, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(player1.ToString());
+        //Debug.Log(player2.ToString());
+        //Debug.Log(tempPlayer.ToString());
+	}
+
+
+    byte[] ObjectToByte(object obj)
+    {
+        if (obj == null)
+        {
+            return null;
+        }
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        using(MemoryStream memoryStream = new MemoryStream())
+        {
+            binaryFormatter.Serialize(memoryStream, obj);
+            return memoryStream.ToArray();
+        }
+    }
+
+    System.Object ByteToObject(byte[] _byte)
+    {
+        MemoryStream memoryStream = new MemoryStream();
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        memoryStream.Write(_byte, 0, _byte.Length);
+        memoryStream.Seek(0, SeekOrigin.Begin);
+        System.Object obj = binaryFormatter.Deserialize(memoryStream);
+        return obj;
+    }
+
+    [Serializable]
+    public class PlayerStat
+    {
+        public int id;
+        public int wheel;
+        public bool steering;
+        public bool motor;
+        public PlayerStat(int _id, int _wheel, bool _steering, bool _motor)
+        {
+            id = _id;
+            wheel = _wheel;
+            steering = _steering;
+            motor = _motor;
+        }
+    }
+
+    //Network
+
+    public void joinConnectionWithUDP()
+    {
+        try
+        {
+            udpClient = new UdpClient(4048);
+            iPEndPoint = new IPEndPoint(IPAddress.Any, 4048);
+            byte[] serverRequestData = udpClient.Receive(ref iPEndPoint);
+            string serverRequest = Encoding.ASCII.GetString(serverRequestData);
+            Debug.Log(serverRequest);
+            Thread threadReceive = new Thread(receivePacketFromServer);
+            StartCoroutine(pingingServer());
+            threadReceive.Start();
+        }
+        catch(Exception ex)
+        {
+            Debug.Log(ex);
+        }
+    }
+
+    IEnumerator pingingServer()
+    {
+        var client = new UdpClient();
+        byte[] sendPacket = new byte[1024];
+        sendPacket = ObjectToByte(Me);
+        client.Send(sendPacket, sendPacket.Length, new IPEndPoint(IPAddress.Parse(iPEndPoint.ToString()), 4048));
+        yield return new WaitForSeconds(1f);
+    }
+
+    IEnumerator sendPacketToServer()
+    {
+        var client = new UdpClient();
+        byte[] sendPacket = new byte[1024];
+        sendPacket = ObjectToByte(Me);
+        client.Send(sendPacket, sendPacket.Length, new IPEndPoint(IPAddress.Parse(iPEndPoint.ToString()), 4050));
+        yield return new WaitForSeconds(0.5f);
+    }
+
+    void receivePacketFromServer()
+    {
+        udpClient = new UdpClient(4050);
+        IPEndPoint tempIPEndPoint = new IPEndPoint(IPAddress.Any, 4050);
+        byte[] receiveData = udpClient.Receive(ref tempIPEndPoint);
+        tempPlayer = (PlayerStat)ByteToObject(receiveData);
+        //filterring
+        Me = tempPlayer;
+        switch (tempPlayer.id)
+        {
+            case 1:
+                {
+                    player1 = tempPlayer;
+                    break;
+                }
+            case 2:
+                {
+                    player2 = tempPlayer;
+                    break;
+                }
+        }
+    }
+
+    public void buttonNgirim()
+    {
+        StartCoroutine(sendPacketToServer());
+    }
+}
---
+++ Assets/Script/Network/ServerUDP.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Threading;
+using System.Net;
+using UnityEngine;
+using System;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine.UI;
+
+public class ServerUDP : MonoBehaviour {
+
+    ClientUDP.PlayerStat player1, player2, tempPlayer;
+    UdpClient serverUDP;
+    List<IPEndPoint> iPEndPoints = new List<IPEndPoint>();
+    public Text debugger;
+
+
+    byte[] ObjectToByte(object obj)
+    {
+        if (obj == null)
+        {
+            return null;
+        }
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        using (MemoryStream memoryStream = new MemoryStream())
+        {
+            binaryFormatter.Serialize(memoryStream, obj);
+            return memoryStream.ToArray();
+        }
+    }
+
+    System.Object ByteToObject(byte[] _byte)
+    {
+        MemoryStream memoryStream = new MemoryStream();
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        memoryStream.Write(_byte, 0, _byte.Length);
+        memoryStream.Seek(0, SeekOrigin.Begin);
+        System.Object obj = binaryFormatter.Deserialize(memoryStream);
+        return obj;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        iPEndPoints.Clear();
+        serverUDP = new UdpClient(4048);
+        Thread broadcastMessage = new Thread(broadcastToClient);
+        broadcastMessage.Start();
+        Thread addNewClient = new Thread(addClient);
+        addNewClient.Start();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void broadcastToClient()
+    {
+        while (iPEndPoints.Count < 2)
+        {
+            serverUDP.Send(Encoding.Default.GetBytes("Selamat Kamu Diterima"), 0, new IPEndPoint(IPAddress.Broadcast, 4048));
+        }
+    }
+
+    void addClient()
+    {
+        while (iPEndPoints.Count < 2)
+        {
+            IPEndPoint tempIPEndPoint = new IPEndPoint(IPAddress.Any, 4048);
+            byte[] serverRequestData = serverUDP.Receive(ref tempIPEndPoint);
+            if(tempIPEndPoint.Address != IPAddress.Parse("192.168.8.101"))
+            {
+                if (iPEndPoints.Count != 0)
+                {
+                    if (iPEndPoints[0] != tempIPEndPoint)
+                    {
+                        iPEndPoints.Add(tempIPEndPoint);
+                        //debugger.text = iPEndPoints.Count + " " + iPEndPoints[iPEndPoints.Count - 1];
+                        Debug.Log(iPEndPoints.Count + " " + iPEndPoints[iPEndPoints.Count - 1]);
+                    }
+                }
+                else
+                {
+                    iPEndPoints.Add(tempIPEndPoint);
+                    //debugger.text = iPEndPoints.Count + " " + iPEndPoints[iPEndPoints.Count - 1];
+                    Debug.Log(iPEndPoints.Count + " " + iPEndPoints[iPEndPoints.Count - 1]);
+                }
+            }
+        }
+        serverUDP.Close();
+        Thread receive = new Thread(receiveFromClient);
+        receive.Start();
+    }
+
+    void receiveFromClient()
+    {
+        serverUDP = new UdpClient(4050);
+        IPEndPoint tempIPEndPoint = new IPEndPoint(IPAddress.Any, 4050);
+        byte[] receiveData = serverUDP.Receive(ref tempIPEndPoint);
+        tempPlayer = (ClientUDP.PlayerStat)ByteToObject(receiveData);
+        debugger.text = "menerima pesan";
+        StartCoroutine(sendToAnotherClient(receiveData, tempIPEndPoint));
+    }
+
+    IEnumerator sendToAnotherClient(byte[] packetData, IPEndPoint iPAnotherClient)
+    {
+        var client = new UdpClient();
+
+        foreach (IPEndPoint ip in iPEndPoints)
+        {
+            if (ip != iPAnotherClient)
+            {
+                client.Send(packetData, packetData.Length, new IPEndPoint(IPAddress.Parse(ip.ToString()), 4050));
+            }
+        }
+        debugger.text = "mengirim pesan";
+        yield return new WaitForSeconds(0.5f);
+    }
+}
