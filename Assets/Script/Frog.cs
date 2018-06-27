ef9f3195b5ed204c44d0848ff97d079b1387ac21
Ahistya Purbolintang <ahistyap@gmail.com>
2018-06-27 22:47:25 +0700

    Added Frog jumping behavior

---
+++ Assets/Script/Frog.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Frog : MonoBehaviour {
+    public float radius;
+    public float timer;
+
+    private Transform target;
+    private NavMeshAgent agent;
+    private float currentTimer;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void OnEnable()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        currentTimer = timer;
+    }
+
+    private void Update()
+    {
+        currentTimer += Time.deltaTime;
+
+        if(currentTimer >= timer)
+        {
+            Vector3 newPosition = RandomNavSphere(transform.position, radius, -1);
+            agent.SetDestination(newPosition);
+            currentTimer = 0;
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float distance, int layerMask)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * distance;
+        randomDirection += origin;
+
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randomDirection, out navHit, distance, layerMask);
+
+        return navHit.position;
+    }
+}
---
+++ Assets/Script/FrogMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogMovement : MonoBehaviour
+{
+
+    public float jumpBehaviour;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(jumpingFrog());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -100)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    IEnumerator jumpingFrog()
+    {
+        transform.Rotate(new Vector3(0, transform.rotation.y + (Random.Range(-55, 55)), 0));
+        yield return new WaitForSeconds(2f);
+        transform.GetComponent<Rigidbody>().AddForce(transform.forward * jumpBehaviour);
+        transform.GetComponent<Rigidbody>().AddForce(transform.up * jumpBehaviour);
+        StartCoroutine(jumpingFrog());
+    }
+}
